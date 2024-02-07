f0086205d28893e33d560f961d1b869e462d81a0
Fabio Rose <fabiorose1811@gmail.com>
2024-02-07 16:56:07 +0100

    added threading to pyaudio stream + logging

--- recorder.py
+++ recorder.py
@@ -1,6 +1,12 @@
 import json
 import os
+import threading
 import time
+from threading import Thread
+
+import socketio
+import logging
+
 import pyaudio
 import numpy as np
 import scipy.io.wavfile as wav
@@ -10,9 +16,11 @@ from typing import List, Optional, Tuple
 
 from webapp.processing.fourier import get_dominant_freq, calc_quality_score, get_dba_level, fft
 
+logging.basicConfig(level=logging.DEBUG)
+
 
 class AudioRecorder:
-    def __init__(self, buffer_size=10, rate=16000, channels=2, chunksize=1024):
+    def __init__(self, buffer_size=10., rate=16000, channels=2, chunksize=1024):
         # "CHUNK" is the (arbitrarily chosen) number of frames the (potentially very long)
         self.chunksize = chunksize
         # channels == 1, only audio input | channels == 2, audio and egg input
@@ -26,6 +34,9 @@ class AudioRecorder:
         self.stream = None
         self.recording_devices = self.__load_recording_devices()
         self.recording_device = None
+        self.stream_thread = None
+        self.stream_thread_is_running = False
+        self.stop_event = threading.Event()
 
     def get_audio_data(self):
         if self.channels == 1:
@@ -37,13 +48,13 @@ class AudioRecorder:
             return None
         return np.hstack(self.frames)[1::2]
 
-
     def __load_recording_devices(self):
         info = self.p.get_host_api_info_by_index(0)
         devices = []
         for i in range(0, info.get('deviceCount')):
             if (self.p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
                 devices.append(self.p.get_device_info_by_host_api_device_index(0, i).get('name'))
+        logging.info("Recording devices loaded successfully.")
         return devices
 
     def __recording_callback(self, input_data, frame_count, time_info, flags):
@@ -51,31 +62,46 @@ class AudioRecorder:
         self.frames.append(frame)
         return input_data, pyaudio.paContinue
 
+    def stream_process(self, input_device_index, cb):
+        stream = self.p.open(format=pyaudio.paInt16,
+                             channels=self.channels,
+                             rate=self.rate,
+                             input=True,
+                             input_device_index=input_device_index,
+                             frames_per_buffer=self.chunksize,
+                             stream_callback=cb)
+        while not self.stop_event.is_set():
+            pass
+        stream.close()
+
     def start_stream(self, input_device_index):
+        if self.stream_thread_is_running:
+            logging.info("Stream is already running.")
+        logging.info("Starting audio stream thread...")
         self.recording_device = input_device_index
-        self.stream = self.p.open(format=pyaudio.paInt16,
-                                  channels=self.channels,
-                                  rate=self.rate,
-                                  input=True,
-                                  input_device_index=input_device_index,
-                                  frames_per_buffer=self.chunksize,
-                                  stream_callback=self.__recording_callback)
-        if self.stream.is_active():
-            print("Stream is active.")
+
+        self.stream_thread_is_running = True
+        self.stream_thread = Thread(target=self.stream_process, args=(input_device_index, self.__recording_callback))
+        self.stream_thread.start()
 
     def stop_stream(self):
-        if self.stream is None:
-            raise RuntimeError("Stream has not been created.")
-        if self.stream is not None and not self.stream.is_active():
-            raise RuntimeError("Stream is currently not active.")
+        if not self.stream_thread_is_running:
+            logging.info("Stream is not currently running.")
+            return
+        logging.info("Stopping audio stream thread...")
+        if self.stream_thread:
+            self.stop_event.set()
+            self.stream_thread.join()
+        self.stream_thread_is_running = False
         self.recording_device = None
-        self.stream.close()
-        print("Stream stopped.")
+        logging.info("Audio stream stopped successfully.")
 
     def stop_stream_and_save_wav(self, save_path):
         self.stop_stream()
-        wav.write(f"{save_path}/out.wav", self.rate, self.get_audio_data())
-        print(f"Stream stopped and file saved at '{save_path}/out.wav'")
+        logging.info("Saving recorded audio buffer...")
+        location = f"{save_path}/out.wav"
+        wav.write(location, self.rate, self.get_audio_data())
+        logging.info(f"Audio buffer saved as {location}")
 
     def terminate(self):
         self.p.terminate()
@@ -91,13 +117,23 @@ class Trigger(AudioRecorder):
                  buffer_size: float = 1.0,
                  channels: int = 1,
                  rate: int = 44100,
-                 chunksize: int = 1024):
+                 chunksize: int = 1024,
+                 socket_event: str = None):
         super().__init__(buffer_size, rate, channels, chunksize)
         self.calib_factors = self.__load_calib_factors(dba_calib_file) if dba_calib_file is not None else None
         self.grid = Grid(semitone_bin_size, dba_bin_size, min_q_score)
         self.__rec_destination = f"{os.path.dirname(os.path.abspath(__file__))}/{rec_destination}"
         # check if trigger destination folder exists, else create
         self.__check_rec_destination()
+        # create a websocket connection
+        self.socket_event = socket_event
+        self.socket = None
+        if socket_event is not None:
+            logging.info("Connecting to websocket server...")
+            self.socket = socketio.Client()
+            # per default connect to local server
+            self.socket.connect("http://localhost:5001")
+            logging.info("Websocket connected successfully.")
 
     def __check_rec_destination(self):
         if os.path.exists(self.__rec_destination):
@@ -105,37 +141,47 @@ class Trigger(AudioRecorder):
         os.makedirs(self.__rec_destination)
 
     def __load_calib_factors(self, dba_calib_file: str) -> dict:
+        logging.info("Loading calibration factors...")
         with open(dba_calib_file) as f:
             corr_factors = json.load(f)
+        logging.info("Calibration factors successfully loaded.")
         return {value[1]: value[2] for value in list(corr_factors.values())}
 
     def start_trigger(self, input_device_index: int):
+        if self.stream_thread_is_running:
+            logging.info("Stream is already running.")
+        logging.info("Starting audio stream thread...")
         self.recording_device = input_device_index
-        self.stream = self.p.open(format=pyaudio.paInt16,
-                                  channels=self.channels,
-                                  rate=self.rate,
-                                  input=True,
-                                  input_device_index=input_device_index,
-                                  frames_per_buffer=self.chunksize,
-                                  stream_callback=self.__trigger_callback)
+
+        self.stream_thread_is_running = True
+        self.stream_thread = Thread(target=self.stream_process, args=(input_device_index, self.__trigger_callback))
+        self.stream_thread.start()
 
     def __trigger_callback(self, input_data, frame_count, time_info, flags):
-        #TODO: Check if emptying frames will lead to better results -> less overlap between trigger
+        # TODO: Check if emptying frames will lead to better results -> less overlap between trigger
         frame = np.frombuffer(input_data, dtype=np.int16)
         self.frames.append(frame)
         if len(self.frames) == self.frames.maxlen:
             data = self.get_audio_data()
             fourier, fourier_to_plot, abs_freq, w = fft(data, self.rate)
-            # print(get_dba_level(data, self.rate))
-            filename = self.grid.add_trigger(get_dominant_freq(abs_freq=abs_freq, w=w),
-                                             get_dba_level(data, self.rate, corr_dict=self.calib_factors),
-                                             calc_quality_score(abs_freq=abs_freq))
+            dom_freq = get_dominant_freq(data, abs_freq=abs_freq, rate=self.rate, w=w)
+            dba_level = get_dba_level(data, self.rate, corr_dict=self.calib_factors)
+            q_score = calc_quality_score(abs_freq=abs_freq)
+            json_data = {"dom_freq": dom_freq, "dba_level": dba_level, "q_score": q_score}
+            logging.info(f"Audio data: {json_data}")
+            self.socket.emit(self.socket_event, json_data)
+            filename = self.grid.add_trigger(dom_freq, dba_level, q_score)
             if filename is not None:
                 wav.write(f"{self.__rec_destination}/{filename}", self.rate, data)
         return input_data, pyaudio.paContinue
 
     def stop_trigger(self):
+        logging.info("Stopping trigger...")
         super().stop_stream()
+        if self.socket is not None and self.socket.connected:
+            self.socket.disconnect()
+            logging.info("Websocket connection closed successfully.")
+        logging.info("Trigger stopped successfully.")
 
 
 class Grid:
@@ -169,17 +215,16 @@ class Grid:
         self.__last_data_tuple = (freq_bin, dba_bin)
         if q_score > self.min_q_score:
             return None
-        print(q_score)
         old_q_score = self.grid[dba_bin - 1][freq_bin - 1]
         if old_q_score is None:
             self.grid[dba_bin - 1][freq_bin - 1] = q_score
-            print(f"new entry for {self.freq_bins_lb[freq_bin - 1]} Q: {q_score}")
+            # print(f"new entry for {self.freq_bins_lb[freq_bin - 1]} Q: {q_score}")
         else:
             if old_q_score > q_score:
                 self.grid[dba_bin - 1][freq_bin - 1] = q_score
-                print(f"updated entry for {self.freq_bins_lb[freq_bin - 1]} old Q: {old_q_score} -> new Q: {q_score}")
+                # print(f"updated entry for {self.freq_bins_lb[freq_bin - 1]} old Q: {old_q_score} -> new Q: {q_score}")
         # return filename for added trigger point
-        return self.__build_file_name(freq_bin-1, dba_bin-1)
+        return self.__build_file_name(freq_bin - 1, dba_bin - 1)
 
     def __build_file_name(self, freq_bin: int, dba_bin: int):
         return f"freqLB_{freq_bin}_dbaLB_{dba_bin}.wav"
@@ -192,7 +237,7 @@ class Grid:
         if self.__last_data_tuple is not None:
             freq, dba = self.__last_data_tuple
             fig.add_trace(
-                go.Scatter(x=[freq-1], y=[dba-1])
+                go.Scatter(x=[freq - 1], y=[dba - 1])
             )
         fig.update_layout(
             xaxis=dict(
@@ -210,5 +255,5 @@ class Grid:
 
 
 if __name__ == "__main__":
-    trigger = Trigger("TEST")
-    trigger.start_trigger(input_device_index=1)
+    trigger = AudioRecorder(channels=1)
+    trigger.start_stream(input_device_index=1)
