6eb8758646bf4da2c97e7e9f9481c735b5b90628
Fabio Rose <fabiorose1811@gmail.com>
2024-02-29 01:00:33 +0100

    updated docstring and typing

--- backend/client.py
+++ backend/client.py
@@ -16,12 +16,22 @@ this.trigger = None
 
 
 @client.on("connect")
-def on_connect():
+def on_connect() -> None:
+    """This function is called when the client successfully connects to the server.
+    It prints a message indicating that the connection has been established.
+    """
     print("connected to server")
 
 
 @client.on("changeSettings")
-def on_settings_change(settings):
+def on_settings_change(settings: dict) -> None:
+    """Function to handle settings change event.
+
+    Parameters
+    ----------
+    settings : dict
+        Dictionary containing the updated settings.
+    """
     logging.info("Creating new trigger instance..")
     this.trigger = Trigger(min_q_score=settings["qualityScore"],
                            semitone_bin_size=settings["frequency"]["steps"],
@@ -40,7 +50,15 @@ def on_settings_change(settings):
 
 
 @client.on("changeStatus")
-def on_status_update(action):
+def on_status_update(action: dict) -> None:
+    """Function to handle status updates. Receives an action dictionary containing actions for the recorder and trigger.
+    Possible actions are: start [trigger & recorder], stop [trigger & recorder], reset [trigger].
+
+    Parameters
+    ----------
+    action : dict
+        Dictionary containing the action trigger.
+    """
     if action["trigger"] == "start":
         # only do smth if trigger is not already running
         if not this.trigger.stream_thread_is_running:
@@ -58,7 +76,14 @@ def on_status_update(action):
 
 
 @client.on("startTrigger")
-def on_start_trigger(device_idx):
+def on_start_trigger(device_idx: int) -> None:
+    """Starts the trigger if it is not already running and emits a status change event.
+
+    Parameters
+    ----------
+    device_idx : int
+        The index of the device to use for recording and triggering.
+    """
     if not this.trigger.stream_thread_is_running:
         this.trigger.start_trigger(device_idx)
     client.emit("statusChanged", {"recorder": "running", "trigger": "running"})
