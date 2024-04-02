a3f6939490ee6fd0dd5d5d6cb97f3f9f9499f49b
Fabio Rose <rosef@hochschule-trier.de>
2024-04-02 13:54:46 +0200

    added file_watcher process to main + gracefully handle keyboardInterrupt

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import logging
 import multiprocessing
-import sys
 import time
 
 from backend.server import run_server
 from backend.client import run_client
+from file_watcher.file_watcher import run_file_watcher
 
 if __name__ == "__main__":
+    processes = []
     try:
-        processes = []
+        proc = multiprocessing.Process(target=run_file_watcher,
+                                       args=("C:\\Users\\fabio\\PycharmProjects\\audio-trigger\\backend\\recordings",))
+        processes.append(proc)
+        proc.start()
 
         proc = multiprocessing.Process(target=run_server)
         processes.append(proc)
@@ -20,6 +24,10 @@ if __name__ == "__main__":
 
         while True:
             time.sleep(1)
+    except KeyboardInterrupt:
+        # Handle keyboard interrupt (Ctrl+C) to gracefully terminate processes
+        for proc in processes:
+            proc.terminate()
     except Exception as e:
         logging.exception(e)
 
