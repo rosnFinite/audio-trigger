e5b542b9bc55906d6b9e86dba96ad06475837a81
Fabio Rose <rosef@hochschule-trier.de>
2024-05-16 13:33:09 +0200

    created csv file now contains additional time column and header

--- src/audio/daq_interface.py
+++ src/audio/daq_interface.py
@@ -118,12 +118,14 @@ class DAQ_Device:
                 task_trig.write([True])
                 task_trig.write([False])
 
-                timestamps = [x/self.sample_rate for x in range(self.num_samples)]
-                data = np.asarray([timestamps, np.array(task_in.read(number_of_samples_per_channel=self.num_samples))])
+                timestamps = np.array([x/self.sample_rate for x in range(self.num_samples)])
+                measurements = np.array(task_in.read(number_of_samples_per_channel=self.num_samples))
+                data = np.hstack((timestamps[:, np.newaxis], measurements[:, np.newaxis]))
             except nidaqmx.errors.DaqError:
                 logger.critical("DAQ process could NOT be started. Check if another program is accessing DAQ resources.")
         if data is not None:
-            np.savetxt(os.path.join(save_dir, "measurements.csv"), data, delimiter=",")
+            header = ",".join(["time"] + self.analog_input_channels)
+            np.savetxt(os.path.join(save_dir, "measurements.csv"), data, delimiter=",", header=header, comments="")
         else:
             with open(os.path.join(save_dir, "measurment_error_info.txt"), "w") as error_file:
                 print("Critical error occured. DAQ measurement process could NOT be started. Check if another program "
